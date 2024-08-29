17b8d8f081d994451f6f098bde89134d46477081
rachelemoscardo <rm4136@columbia.edu>
2024-08-29 18:07:30 -0400

    Update Week 1 Files

--- Lectures-gh/Lecture1-gh/Lecture1-startclass.R
+++ Lectures-gh/Lecture1-gh/Lecture1-startclass.R
@@ -3,7 +3,7 @@
 ## [ PROJ ] Lecture1-startclass: Getting familiar with RStudio
 ## [ FILE ] Lecture1-startclass.r
 ## [ AUTH ] INSTRUCTOR FILE 
-## [ INIT ] Jan 16, 2024 
+## [ INIT ] Sep 3, 2024 
 ##
 ################################################################################
 
@@ -13,21 +13,21 @@
 
 
 ## -----------------------------------------------------------------------------
-#1. look around and get our bearings.
+## 1. look around and get our bearings.
 ## -----------------------------------------------------------------------------
 
-#get working directory
-  getwd()
+# get working directory
+
 
 
 ## -----------------------------------------------------------------------------
 ## 2. install and load the gapminder package 
 ## -----------------------------------------------------------------------------
 
-#first we have to install the package
-  install.packages("gapminder")
+# first we have to install the package
+  
 
-#load this package
+# load this package
 
 
 ## -----------------------------------------------------------------------------
@@ -35,37 +35,37 @@
 ##    (this exercise is based on STAT545 by Jenny Bryan)
 ## -----------------------------------------------------------------------------
 
-#let's assign the gapminder data frame to a new data frame called gap
+# let's assign the gapminder data frame to a new data frame called gap
 
 
-#let's use some functions to inspect gapminder dataframe (an object in the gapminder package)
+# let's use some functions to inspect gapminder dataframe (an object in the gapminder package)
 
-  #access built in help function for str()
+  # access built in help function for str()
    
 
-  #the function class() tells us what class(es) an object is assigned to
+  # the function class() tells us what class(es) an object is assigned to
     
     
-  #some other functions
+  # some other functions
     
 
 ## -----------------------------------------------------------------------------
 ## 4. Use some base R functions to perform some very basic exploratory analysis
 ## -----------------------------------------------------------------------------
 
-#let's use some more base R functions to understand the data structure
+# let's use some more base R functions to understand the data structure
 
 
-#assign the number of columns in gap to a new object 
+# assign the number of columns in gap to a new object 
   
   
-#get summary statistics
+# get summary statistics
 
 
-#let's plot the relationship of year (x) vs lifeExp (y) using base R
+# let's plot the relationship of year (x) vs lifeExp (y) using base R
 
 
-#what is a factor? let's look at some base R functions to figure it out
+# what is a factor? let's look at some base R functions to figure it out
 
   
   
