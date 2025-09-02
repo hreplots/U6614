7d3a84bf2ad8db16b97c240b43497b1d2568ffd5
Nico Rojas <43378796+Nico-Rojas@users.noreply.github.com>
2025-09-02 14:33:29 -0400

    Add files via upload

--- Lectures-gh/Lecture1-gh/Lecture1-startclass.R
+++ Lectures-gh/Lecture1-gh/Lecture1-startclass.R
@@ -3,7 +3,7 @@
 ## [ PROJ ] Lecture1-startclass: Getting familiar with RStudio
 ## [ FILE ] Lecture1-startclass.r
 ## [ AUTH ] INSTRUCTOR FILE 
-## [ INIT ] Jan 21, 2025 
+## [ INIT ] Sep 21, 2025 
 ##
 ################################################################################
 
@@ -11,27 +11,31 @@
 ## 0. create an R project that includes this R script (Lecture1-startclass.r)
 ## -----------------------------------------------------------------------------
 
+# this is an R script!
+# an R script is just a bunch of text saved in a file with the .R extension
+#   that RStudio knows to be R code (along with code comments like this)
+
+# first create a project by selecting File -> New Project
+# - when you create or open a project, RStudio will reload the workspace
+# - revisit your R script by opening the .rproj file in the same directory
+
+
+# let's check the working directory by executing the getwd() function
+getwd()
+
 
 ## -----------------------------------------------------------------------------
-## 1. look around and get our bearings.
+## 1. install and load the gapminder package 
 ## -----------------------------------------------------------------------------
 
-# get working directory
-
-
-
-## -----------------------------------------------------------------------------
-## 2. install and load the gapminder package 
-## -----------------------------------------------------------------------------
-
-# first we have to install the package
+# first we have to install the package (once and only once!)
   
 
 # load this package
 
 
 ## -----------------------------------------------------------------------------
-## 3. Inspect gapminder data frame (in the gapminder package) w base R functions
+## 2. Inspect gapminder data frame (in the gapminder package) w base R functions
 ##    (this exercise is based on STAT545 by Jenny Bryan)
 ## -----------------------------------------------------------------------------
 
@@ -42,15 +46,11 @@
 
   # access built in help function for str()
    
-
-  # the function class() tells us what class(es) an object is assigned to
-    
-    
-  # some other functions
+  # here are some other inspection functions
     
 
 ## -----------------------------------------------------------------------------
-## 4. Use some base R functions to perform some very basic exploratory analysis
+## 3. Use some base R functions to perform some very basic exploratory analysis
 ## -----------------------------------------------------------------------------
 
 # let's use some more base R functions to understand the data structure
