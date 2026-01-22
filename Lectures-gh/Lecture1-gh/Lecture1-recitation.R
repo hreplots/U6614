50bc4942d8ba8f9ed4ee66d64fa7aa6bc7ce8580
fmichielli <fcm2118@columbia.edu>
2026-01-22 18:30:13 -0500

    Add files via upload

---
+++ Lectures-gh/Lecture1-gh/Lecture1-recitation.R
@@ -0,0 +1,402 @@
+################################################################################
+##
+## [ PROJ ] < Caste-based expenditure gaps and the ACCESS 2018 Survey >
+## [ FILE ] < Lecture1-startclass.R >
+## [ INIT ] < Jan 20, 2026 >
+##
+################################################################################
+
+## ---------------------------
+## LIBRARIES
+## ---------------------------
+
+# install packages once and only once!
+# remember to comment out or remove install code after you run it once!
+#install.packages('tidyverse')
+#install.packages('DescTools)
+
+# load packages every time you start a new RStudio session
+library(tidyverse)
+library(DescTools)
+
+
+## ---------------------------
+## DIRECTORY PATHS
+## ---------------------------
+
+getwd()
+
+
+## -----------------------------------------------------------------------------
+## 1. load and inspect access2018 data: 
+##    
+##    a. inspect the data frame and data types for each column
+##        - make sure to inspect the age, gender, caste, income source, state & educ columns
+##
+##    b. use the mutate function to create new column for gender:
+##        - gender.fac = as.factor(gender),
+##        - check if it worked
+##
+##    c. in a single pipe:
+##        - include gender.fac in a new data frame 
+##        - call the new data frame access2018.temp1, 
+##        - also create factors for caste, income source, state and education,
+##        - exclude the columns for HHID and date
+##       after creating access2018.temp1, print the first 5 observations
+##
+##    d. inspect caste.fac, gender.fac, incsource.fac, education.fac and state.fac
+##.      using the levels() function,
+##       what package is the levels() function located in?
+##
+##    e. use filter() to only include rows only for the state Uttar Pradesh,
+##       store as a new object access_UP,
+##       print the first 5 observations,
+##       confirm your data only includes observations for Uttar Pradesh
+##
+##.   f. what is the female share of respondents in Uttar Pradesh? 
+##       compare it to the female share of respondents in Bihar. 
+##
+##    g. remove the access2018.temp1 object from memory using the rm() function
+## -----------------------------------------------------------------------------
+
+# 1a. 
+
+  load("access2018.RData")
+  str(access2018)
+  
+  # View(access2018)
+  # we can also inspect the data frame by double-clicking in the Environment tab
+  # NOTE: NEVER include View() in your R Markdown file!
+  #       if you do your knit document will print the entire data frame!
+
+  summary(access2018$age)
+  
+  # note how summary is not very useful with character variables
+  summary(access2018$gender) 
+  summary(access2018$caste) 
+  summary(access2018$education) 
+  summary(access2018$state)
+  
+  # we need to coerce these characters into factors to see their distribution!
+
+  
+# 1b.
+  # mutate() is a tidyverse function, good practice to initialize with a pipe!
+  access2018 %>% mutate(gender.fac = as.factor(gender))
+  
+  # note: the output is an object, we're just not assigning it to the environment
+  # but we can put the entire operation within str() to inspect the output
+  str(mutate(access2018, gender.fac = as.factor(gender))) 
+  summary(mutate(access2018, gender.fac = as.factor(gender))$gender.fac) 
+  
+  
+# 1c. 
+  access.temp1 <- access2018 %>% 
+    mutate(gender.fac = as.factor(gender),
+          caste.fac = as.factor(caste),
+          education.fac = as.factor(education), 
+          state.fac = as.factor(state),
+          incsource.fac = as.factor(incsource)) %>% 
+    select(-HHID, -date) 
+
+  head(access.temp1, n = 5) 
+
+  # some helpful base R syntax for later: 
+  
+    # subset the first row of access.temp1
+    access.temp1[1,]
+    
+    # subset the cell in the first row, 5th column (i.e. first obs for age)
+    access.temp1[1,5]
+
+
+# 1d.
+  levels(access.temp1$gender.fac)
+  levels(access.temp1$caste.fac)
+  levels(access.temp1$education.fac)
+  levels(access.temp1$state.fac)
+  levels(access.temp1$incsource.fac)
+
+  ?levels   
+  # note that levels is a base R function
+  # so levels can't be used with columns using tidyverse syntax (i.e. not within a pipe)
+  # make sure you understand why this won't work: 
+  #  cps.temp1 %>% levels(sex.fac)
+
+
+# 1e.
+  access_UP <- access.temp1 %>% 
+    filter(state.fac == "UTTAR PRADESH")
+  
+  access_UP %>% head(n = 5)
+  
+  # validate
+  summary(access_UP$state.fac)
+
+  
+# 1f.
+
+  #Share of female respondents in UP
+  summary(access_UP$gender.fac)
+  table(access_UP$gender.fac)
+  prop.table(table(access_UP$gender.fac))[1]
+
+  
+  # let's round to 3 decimals
+  # output from prop.table() is just a vector, can pass it into a pipe to round
+  prop.table(table(access_UP$gender.fac)) %>% round(3)
+  
+  # but...
+  # in the write-up of your RMD file you'll enter code to return the female share
+  # don't hardcode numbers in your RMD write-up
+  # in your RMD file, you generally can't use pipes with in-line code references
+  # so here is base R syntax to round as an in-line code reference
+  round(prop.table(table(access_UP$gender.fac)), 3)
+  
+  # syntax for in-line references
+  
+  t1_UP <- prop.table(table(access_UP$gender.fac))
+  t1_UP1 <- t1_UP[1] # access the first element of this table
+  
+ # `r t1_UP`
+ # `r round(t1_UP[1], 3)`
+  
+  # can also the Desc() function in the DescTools package
+  Desc(access_UP$gender)
+  
+  # OPTIONAL advanced exercise (something to try on your own if you have time): 
+  
+    # what type of object is the output of Desc()
+    str(Desc(access_UP$gender)) # a list! 
+  
+    # actually...
+    # it's a list with another list as the only element of the list (weird!)
+    str(Desc(access_UP$gender)[[1]])
+    Desc(access_UP$gender)[[1]]
+  
+    # how can you refer directly to the female share?
+    
+    # first find which element of the list is the frequency table
+    Desc(access_UP$gender)[[1]]$freq
+    
+    # this object is a matrix, now just point to the right element of the matrix!
+    round(Desc(access_UP$gender)[[1]]$freq[2,3], 3)
+  
+
+  #Share of female respondents in Bihar
+  access_bihar <- access.temp1 %>% 
+    filter(state == "BIHAR")
+  summary(access_bihar$gender.fac)
+  prop.table(table(access_bihar$gender.fac))
+  
+
+# 1g.
+  rm(access.temp1)
+  rm(access_bihar)
+
+
+## -----------------------------------------------------------------------------
+## 2. Describe the access_UP data frame
+##
+##    a. what is the unit of observation (or unit of analysis)?
+##
+##    b. how many individuals are observed? 
+##       how many of these are from 'Reserved' castes (i.e: Scheduled Caste, Scheduled Tribe, Other Backward Class")? 
+##       how many are "General" caste members?
+##       [Note: for this question, use the 'table' and 'prop.table' functions. 
+##             'table' will give the number of observations belonging to each caste category.
+##             'prop.table will give the proportion]
+##
+##    c. do you think the sample is representative of all households in India?
+##       how would you describe the population represented by this sample?
+##       is there more information you would like to see to assess the representativeness of the sample?
+##
+##    d. what is the average age of individuals in the sample? youngest and oldest person?
+## -----------------------------------------------------------------------------
+
+# 2b. Note: For this question, first run the following code to add a dummy 
+# variable 'reserved' to the 'access_UP' dataframe. This variable has two levels— 
+# 'Other Backward Class', 'Scheduled Caste' and 'Scheduled Tribe' belong to the 
+# 'Reserved' level, while the rest are assigned to 'General'.
+  access_UP <- access_UP %>% 
+    mutate(reserved = factor(if_else(caste.fac %in% c("Other Backward Class", 
+                                                      "Scheduled Caste",
+                                                      "Scheduled Tribe"),
+                                     "Reserved",
+                                     "General")))
+
+  #Number of total observations in access_UP
+  
+  nrow(access_UP)
+  
+  #Number of 'Reserved' and 'General' individuals
+  table(access_UP$reserved)
+  
+  #Proportion of 'Reserved' and 'General' individuals
+  round(prop.table(table(access_UP$reserved)), 3)
+    
+
+# 2d. 
+  summary(access_UP$age)[4]
+  
+  # `r round(summary(access_UP$age)[4], 2)`
+
+
+## -----------------------------------------------------------------------------
+## 3. Let's now look at expenditures per month for different groups in Uttar Pradesh
+##
+##    a. find the observation for the top monthly expenditure using the summarize() function, 
+##        assign this to a new object called max_exp_obs1
+##
+##    b. find max monthly expenditures using the arrange function instead of summarize
+##
+##    c. use the filter function to subset for the observation with max monthly expenditure
+##        (don't hardcode the max expenditure to filter on, refer to the max_exp_obs1 object from a),
+##        store in new data frame max_exp_obs2,
+##        confirm it worked
+##
+##    d. what is the age, gender and caste of the top monthly spender in the sample?
+##
+##    e. list the age, gender and caste of the top 10 monthly spenders in the sample.
+##
+##    f. how many individuals spend more than the mean monthly expenditure amount of the sample?
+## -----------------------------------------------------------------------------
+
+# 3a. 
+max_exp_obs1 <- access_UP %>% 
+    summarize(max_exp = max(month_exp))
+  
+max_exp_obs1
+  
+# 3b. 
+
+  access_UP %>% 
+    arrange(desc(month_exp)) %>% 
+    select(month_exp) %>% 
+    head(n = 1)
+  
+# 3c. 
+# HINT: your condition needs to refer to the max monthly expenditures (month_exp)
+# you created max_exp_obs1 as a data frame in part a,
+# so in your filter() call refer to the value from max_exp_obs1 that you want to filter on
+# this requires subsetting the appropriate element from that data frame
+# (in this case just the max_exp column of the max_exp_obs1 data frame)
+  
+  max_exp_obs2 <- access_UP %>% 
+    filter(month_exp == max_exp_obs1$max_exp)
+  
+  #validate
+summary(max_exp_obs2$month_exp)
+    
+# 3d.
+
+max_exp_obs2 %>% 
+  select(age, gender, caste) %>% 
+  head(n=1)
+
+# 3e.
+access_UP %>% 
+  arrange(desc(month_exp)) %>% 
+  select(age, gender, caste, month_exp) %>% 
+  head(n=10)
+  
+# 3f.
+access_UP %>% 
+  filter(month_exp > mean(month_exp)) %>% 
+  nrow()
+
+  
+## -----------------------------------------------------------------------------
+## 4. Now, let's look at caste-based monthly expenditure gaps in Uttar Pradesh. 
+##    [Note: Since this data has no earnings data, we are using monthly expenditure 
+##    (month_exp) as a proxy for earnings.]
+##
+##    a. use the filter function and 'reserved' dummy variable to subset observations belonging to the 'General' caste, 
+##       assign to new data frame, access_UPgen,
+##       sort in descending order of monthly expenditure
+##       check if it worked
+##
+##    b. repeat part a, but this time, use the filter function to subset observations 
+##       belonging to 'Reserved" castes.
+##       assign them to a new data frame called 'access_UPres'. 
+##
+##    c. use summarise to find mean, min & max monthly expenditure for the General 
+##       category and Reserved category of castes, separately.
+##       name each statistic appropriately (i.e. name each column in the 1-row table of stats)
+##       what is the gap in mean monthly expenditure between the two groups? 
+##
+##    d. research suggests that people belonging to the 'General' caste category
+##       own 65% of agricultural land in India (India Human Development Survey, 2020). 
+##       they are also more likely to own land in a proportion that is much higher
+##       than their share of the population. 
+##       we will now use this data to compare 'General' versus 'Reserved' households
+##       using the 'reserved' dummy variable to understand whether this is true. 
+##       - use the 'table' and 'prop.table' functions to explore the total number 
+##         of landowners by their caste. 
+##       - here, 'landowners' are people whose primary source of income is 
+##         agriculture on their own land. So, you can use the 'incsource.fac' 
+##         variable to filter the 'access_UP' data frame for 'Agriculture (own land)'
+##       - does the number of General and Reserved caste landowners seem 
+##         proportional to the overall number of 'General' and 'Reserved' caste 
+##         individuals in Uttar Pradesh's population (as calculated in Q2 b)? 
+##
+##    e. do differences in landowning explain the expenditure gap between General and Reserved castes?
+##       i.e., does the General-Reserved expenditure gap persist among landowning families?
+##       what is the gap in mean monthly expenditures of General vs Reserved category landowners? 
+##
+##    f. is there a gap between the mean monthly expenditures of General caste male 
+##       and Reserved caste male landowners? what about the same, but for the female landowners? 
+##
+##    g. does educational attainment explain part of the expenditure gap 
+##       between General and Reserved caste landowners?
+##       what is gap between landowning General vs. Reserved household with a HS degree or more? 
+## -----------------------------------------------------------------------------
+
+# 4a. 
+
+access_UPgen <- access_UP %>% 
+  filter(reserved == "General") %>% 
+  arrange(desc(month_exp))
+  
+  
+  #validate
+  summary(access_UPgen$reserved)
+  
+# 4b. 
+
+  
+  #validate
+
+
+# 4c.
+
+
+# 4d.
+
+
+# 4e. 
+
+
+ 
+## Is this difference statistically significant?
+## It is critical to test for significance before emphasizing differences in means.
+## we will cover the functions to do inference in Week 4
+
+
+
+# 4f. 
+
+
+  
+# 4g. 
+# Note: First run the following code to create a dummy variable 'degree' with two levels: 
+# "HS degree or above" and "Less than HS".
+  access_UP <- access_UP %>% 
+    mutate(degree = factor(if_else(education.fac %in% c("Grade 12", 
+                                                         "Graduate or above"),
+                                    "HS degree or above",
+                                    "Less than HS")))
+  
+  # General v Reserved landowners who have degrees
+
+  
